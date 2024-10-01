dcf87d5d3e1dbce5d2183f3282cbfb9de6db23da
Wender <wendermaiac@gmail.com>
2024-10-01 08:00:58 -0300

    .

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -2,18 +2,18 @@ import { PostInput } from "@/components/input";
 
 export default function Home() {
     return (
-        <div className="flex justify-center w-screen h-screen bg-neutral-background">
-            <div className="w-[90%] h-full dev" >
-                <header className=" grid grid-cols-[30%,40%,30%] h-[80px] dev">
-                    <div className="dev">30%</div>
-                    <div className="dev">40%</div>
+        <div className="flex justify-center w-screen bg-neutral-background">
+            <div className="w-[90%] h-screen dev" >
+                <header className=" grid grid-cols-[28%,44%,28%] h-[10%] dev">
+                    <div className="dev">27,5%</div>
+                    <div className="dev">45%</div>
                     <div className="dev">30%</div>
                 </header>
 
-                <div class="grid grid-cols-[30%,40%,30%] ">
-                    <div className="dev">30%</div>
-                    <div className="dev">40%</div>
-                    <div className="dev">30%</div>
+                <div class="grid grid-cols-[28%,44%,28%] h-[90%] ">
+                    <div className="h-full dev" >30%</div>
+                    <div className="overflow-y-scroll dev">40%</div>
+                    <div className="h-full dev">30%</div>
                 </div>
 
             </div>
