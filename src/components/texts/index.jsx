44a73a3711ab518636b21dea0a35832825a29986
Wender <wendermaiac@gmail.com>
2024-10-02 08:37:41 -0300

    new: função para criar posts

--- src/components/texts/index.jsx
+++ src/components/texts/index.jsx
@@ -1,10 +1,18 @@
 import next from "next";
 
 export const Text = (props) => {
-  return <p className={` ${props.children}`}>{props.children}</p>;
+  return <p className={` ${props.style}`}>{props.children}</p>;
 };
 
-export const ProfileName = ({nomeExibicao, nomeUsuario}) => {
+export const ButtonText = (props) => {
+  return <p className={` text-neutral-background  ${props.style}`}>{props.children}</p>;
+};
+
+export const MediumText = (props) => {
+  return <p className={` text-[0.875rem] ${props.style}`}>{props.children}</p>;
+};
+
+export const ProfileName = ({ nomeExibicao, nomeUsuario }) => {
   return (
     <div className={`flex flex-col px-2`}>
       <h2 className={``}>{nomeExibicao}</h2>
