66bc6acb55245a8de7d23409cad496a3bbe1ee17
Marcelo <marcelocardoso020304@gmail.com>
2024-10-02 08:40:17 -0300

    iniciado imagens do post

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -5,10 +5,14 @@ import { FaRegComment } from "react-icons/fa";
 import { FiTrash } from "react-icons/fi";
 import { ProfileName, Text } from "../texts";
 import { PostButton } from "../postButton";
+import Image from "next/image";
 
 export const Post = (props) => {
+  const imageLoader = ({ src, width, quality }) => {
+  return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+}
   return (
-    <article className="w-[85%] h-[45%] px-5 py-5 flex flex-col justify-between rounded-2xl bg-neutral-900 bg-opacity-50 dev">
+    <article className="w-[85%] h-fit px-5 py-5 flex flex-col justify-between rounded-2xl bg-neutral-900 bg-opacity-50 dev">
       {/* user infos */}
       <div className="h-[10%] w-[100%] flex">
         <img
@@ -18,25 +22,33 @@ export const Post = (props) => {
         />
         <ProfileName nomeExibicao={"Fulano da Silva"} nomeUsuario={"@Fulano"} />
 
-        <PostButton fieldStyle="w-[25%] ">
-          <FiTrash style={{ scale: "130%", paddingTop: "2%" }} />
-          Deletar
-        </PostButton>
-
         <span className=" flex text-center gap-2 opacity-50 text-sm py-1">
           <FaRegClock style={{ scale: "130%", paddingTop: "2.5%" }} />
           32 seg atras
         </span>
+
+        <PostButton fieldStyle="w-[25%] ">
+          <FiTrash style={{ scale: "130%", paddingTop: "2%" }} />
+          Deletar
+        </PostButton>
       </div>
 
       {/* post content */}
-      <div className="">
+      <div className="w">
         <Text>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, quod!
           Delectus maiores praesentium aspernatur, sequi aperiam voluptatibus
           ipsa quo. Consequuntur maiores ipsa ratione quod? Fuga aliquid illo
           iure sint quia.
         </Text>
+
+        <div className="w-[70%] h-[70%]">
+        <img
+          className="w-[60%] h-[70%] rounded-2xl"
+          src="https://i.pinimg.com/originals/0c/bb/31/0cbb31514710d619571766987c0670c6.jpg"
+          alt="imagem de perfil do usuário logado"
+        />
+        </div>
       </div>
 
       {/* likes-comentarios */}
